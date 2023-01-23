747d9654097a40716bbd5541d37f368479c6c50c
Mehmet <gundogan.mehmet33@gmail.com>
2023-01-23 19:12:18 +0300

    contack send mail hazırlandı

--- resources/views/components/frontend/about.blade.php
+++ resources/views/components/frontend/about.blade.php
@@ -3,27 +3,30 @@
         <div class="flex flex-col xl:flex-row gap-24">
             <img class="object-cover h-full w-[500px] md:mx-auto lg:mx-0 rounded-2xl" src="/img/aboutoguz.png"
                 alt="Hakkimda Resim">
-                <div class="flex flex-col items-center text-center lg:items-start lg:text-left">
-                    <div class="flex flex-col">
-                        <h2 class="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3">
-                            Hakkımda
-                        </h2>
-                        <p class="mb-4 text-accent">Full Stack Developer</p>
-                        <hr class="mb-8 opacity-90 dark:opacity-5" />
-                        <p class="mb-8">
-                            Lorem ipsum dolor sit amet consectetur adipisicing elit tempore
-                            earum dolorum quo consequatur rem nihil hic ducimus rerum soluta
-                            neque harum velit molestiae dignissimos distinctio dolorum nisi
-                            labore culpa nihil. <br />
-                            <br />
-                            Qui doloremque quod sit accusantium unde totam inventore
-                            cupiditate ratione esse maxime eum recusandae ducimus.
-                        </p>
-                    </div>
-                    <a href="#contact" class="btn btn-md bg-accent text-light-tail-100">
-                        Benimle Çalış
-                    </a>
+            <div class="flex flex-col items-center text-center lg:items-start lg:text-left">
+                <div class="flex flex-col">
+                    <h2 class="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3">
+                        Hakkımda
+                    </h2>
+                    <p class="mb-4 text-accent">Full Stack Developer</p>
+                    <hr class="mb-8 opacity-90 dark:opacity-5" />
+                    <p class="mb-8">
+                        5 yıldır yazılım geliştirme bölümündeyim ve 5 kişilik kendi ekibim var. Özellikle Php
+                        yazılımları geliştirmekle ilgileniyorum. Tam donanımlı web geliştiricisi. Yazılımların
+                        sınırlarını zorlamayı seviyorum. Yeniliklere açık fikirli. <br />
+                    <p class="mb-4 text-accent">Eğitim</p>
+                    Çukurova ÜniversitesiÇukurova Üniversitesi
+                    ön lisans, Bilgisayar Programlama
+                    <br>
+                    Lefke Avrupa Üniversitesi (LAÜ)Lefke Avrupa Üniversitesi (LAÜ)
+                    Lisans Derecesi, Computer Engineer
+                    </p>
+                    <br>
                 </div>
+                <a href="#contact" class="btn btn-md bg-accent text-light-tail-100">
+                    Bana Ulaş
+                </a>
+            </div>
         </div>
     </div>
 </section>
